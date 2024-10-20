18b0160c46f2e4a7b328cc94c1831272ac3e9ad2
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 17:30:52 +0700

    clear: palindrome using regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const filtered = text
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{1,}/g, "");
 
+  const reversed = filtered.split("").reverse().join("")
+  
+  return filtered === reversed;
 };
 
 // Do not edit below this line
