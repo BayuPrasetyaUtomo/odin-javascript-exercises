d0849a32eb32c2630287fcb371fd5cbb56a5a7f8
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 19:08:16 +0700

    clear: find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+// use current year if the yearOfDeath is undefined
+const getAge = (birth, death) => (death ?? new Date().getFullYear()) - birth;
 
+const findTheOldest = function (people) {
+  let oldest = [];
+
+  for (person of people) {
+    if (!person.yearOfBirth) continue;
+
+    const age = getAge(person.yearOfBirth, person.yearOfDeath);
+
+    if (!oldest.length) oldest.push(person.name, age)
+    if (age > oldest.at(-1)) oldest = [person.name, age];
+  }
+  return people.find(person => person.name === oldest.at(0))
 };
 
 // Do not edit below this line
