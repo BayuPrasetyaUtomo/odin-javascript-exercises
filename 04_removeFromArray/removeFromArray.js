23c174bcb5bdd024ee2a61b45930ea27b29e4c33
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 14:06:55 +0700

    feat: remove given value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayOfValue = [], ...values) {
+  for (v of values) {
+    arrayOfValue = arrayOfValue.filter(item => item !== v)
+  }
+
+  return arrayOfValue;
 };
 
+// console.log(removeFromArray([1, 2, 2, 4], 2))
 // Do not edit below this line
 module.exports = removeFromArray;
