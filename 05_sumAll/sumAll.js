67209f95c91a7bbb638c7cb9d75719f13150b29a
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 14:26:25 +0700

    clear: test-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  let result = 0;
 
+  const isNumber = (num) => typeof num === "number";
+  const isFloat = (num) => num % 1 !== 0;
+
+  if (!isNumber(endNum) || startNum < 0 || isFloat(startNum)) return "ERROR";
+  
+  if (startNum > endNum) {
+    for (let i = startNum; i >= endNum; i--) {
+      result += i;
+    }
+    return result;
+  } else {
+    for (let i = startNum; i <= endNum; i++) {
+      result += i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
