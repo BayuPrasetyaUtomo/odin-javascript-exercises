88c09815b76009943a3038e6ddf1062ee7027137
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 12:56:59 +0700

    feat: function to repeat string n number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimes) {
+  let repeatedString = "";
+  for (let index = 0; index < numberOfTimes; index++) {
+    repeatedString += stringToRepeat;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
