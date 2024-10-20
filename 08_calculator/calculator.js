97e6cdd2cd0e26e1a41fa226cc055905e5878c39
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-20 16:39:57 +0700

    clear: calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums = []) {
+  let total = 0;
+  for (n of nums) {
+    total += n;
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (nums = []) {
+  let total = 0;
+  
+  for (n of nums) {
+    if (total === 0) total += n;
+    else total *= n;
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
